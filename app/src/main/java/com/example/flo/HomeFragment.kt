e94ab8049c4494ef52e1df0d7be2c3b411082fed
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-11-03 03:24:20 +0900

    2022.11.03 - 뷰페이저 적용

--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,7 +21,12 @@ class HomeFragment : Fragment() {
         binding.homePannelAlbumImgDetailIv.setOnClickListener{
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
         }
-
+        //뷰페이저
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 좌우 스크롤
         return binding.root
     }
 }
